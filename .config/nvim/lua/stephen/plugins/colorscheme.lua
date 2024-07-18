c977b4fc2260276a3fbddc279faf95deaa5351c5
clarks03 <stephen.clark@mail.utoronto.ca>
2024-07-18 19:11:59 -0400

    first commit

---
+++ .config/nvim/lua/stephen/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+
+    -- nord
+    -- "shaunsingh/nord.nvim",
+    -- priority = 1000,
+    -- config = function()
+    --     vim.cmd("colorscheme nord")
+    -- end
+
+    -- gruvbox
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme gruvbox")
+    end
+
+}
